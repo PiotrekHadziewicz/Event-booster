4bc317117b8dc100bae0b871a3022d1a50b0beca
Aleksander Lasek <aleksanderlasek01@gmail.com>
2022-04-25 19:59:54 +0200

    działąjący przycisk zamykania + otwieranie całym div

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,25 +1,20 @@
-const $mainEvents = document.querySelectorAll('.main-events');
+const $mainEvents = document.querySelector('.main-field');
 const $backdrop = document.querySelector('.backdrop');
 const $modal = document.querySelector('.modal');
 const $closeModalBtn = document.querySelector('.modal__close-btn');
 
 function openModal(event) {
-  if (event.code == 'Escape') {
-    console.log('wcisniety esc');
-    event.preventDefault();
-    $backdrop.hidden = false;
-    $modal.hidden = false;
-  }
+  event.preventDefault();
+  console.log(event.target);
+  $backdrop.hidden = false;
+  $modal.hidden = false;
 }
 
 function closeModal(event) {
-  if (event.code == 'Space') {
-    console.log('wcisniety space');
-    event.preventDefault();
-    $backdrop.hidden = true;
-    $modal.hidden = true;
-  }
+  event.preventDefault();
+  $backdrop.hidden = true;
+  $modal.hidden = true;
 }
 
-document.addEventListener('keydown', openModal);
-document.addEventListener('keydown', closeModal);
+$mainEvents.addEventListener('click', openModal);
+$closeModalBtn.addEventListener('click', closeModal);
