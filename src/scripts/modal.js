7bbeba9dde187f48dc838c1539059f41ff5b6030
Aleksander Lasek <aleksanderlasek01@gmail.com>
2022-04-25 19:36:59 +0200

    test modala

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -3,15 +3,23 @@ const $backdrop = document.querySelector('.backdrop');
 const $modal = document.querySelector('.modal');
 const $closeModalBtn = document.querySelector('.modal__close-btn');
 
-function openModal() {
-  $backdrop.hidden = false;
-  $modal.hidden = false;
+function openModal(event) {
+  if (event.code == 'Escape') {
+    console.log('wcisniety esc');
+    event.preventDefault();
+    $backdrop.hidden = false;
+    $modal.hidden = false;
+  }
 }
 
-function closeModal() {
-  $backdrop.hidden = true;
-  $modal.hidden = true;
+function closeModal(event) {
+  if (event.code == 'Space') {
+    console.log('wcisniety space');
+    event.preventDefault();
+    $backdrop.hidden = true;
+    $modal.hidden = true;
+  }
 }
 
-$mainEvents.addEventListener('click', openModal);
-$closeModalBtn.addEventListener('click', closeModal);
+document.addEventListener('keydown', openModal);
+document.addEventListener('keydown', closeModal);
